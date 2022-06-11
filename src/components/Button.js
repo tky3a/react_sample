51ef7366a3fa0cd6087e43d46c2e94625f34082d
tky <takayamac@icloud.com>
2022-06-12 02:00:36 +0900

    style-componentsでボタンコンポーネント作成

---
+++ src/components/Button.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+// デフォルトボタン(propsによって色を変える)
+const Button = styled.button.attrs(({ color = '#007bff' }) => {
+  console.log('propsでcolorを受け取る', color);
+  return ({
+    background: color,
+  });
+})`
+font-size: 1em;
+  color: white;
+  padding: 5px 10px;
+  border-radius: 5px;
+  cursor: pointer;
+  border: none;
+  background: ${(color) => color};
+  &:hover {
+    opacity: 0.7;
+  }
+  &:active {
+    opacity: 0.2;
+  }
+`;
+
+export default Button;
--- src/pages/login/Index.js
+++ src/pages/login/Index.js
@@ -1,5 +1,13 @@
+import Button from '../../components/Button';
+
+// TODO: reduxとカスタムフック
 const Login = () => (
-  <h2>Login Page</h2>
+  <>
+    <h2>Login Page</h2>
+    <div>
+      <Button color="blue" onClick={() => { console.log(11234); }}>ログイン</Button>
+    </div>
+  </>
 );
 
 export default Login;
