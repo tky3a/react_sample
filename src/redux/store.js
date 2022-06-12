7f39c763c19bf7e0ecef1e97a824b79099283362
tky <takayamac@icloud.com>
2022-06-12 11:49:55 +0900

    storeでreducerを設定

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
+import rootReducer from './reducers';
 
 const store = configureStore({
-  reducer: {},
+  reducer: rootReducer, // reducerを設定
 });
 
 export default store;
